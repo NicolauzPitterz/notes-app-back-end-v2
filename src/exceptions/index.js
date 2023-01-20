c7799978f8f9e2e49811f1d13b1a5486c3c353dc
Pitters Nicolaus Tulungen <nicolauzpitterz@gmail.com>
2023-01-20 10:39:20 +0800

    authorization implemented

--- src/exceptions/index.js
+++ src/exceptions/index.js
@@ -1,5 +1,13 @@
 const ClientError = require("./ClientError");
 const InvariantError = require("./InvariantError");
 const NotFoundError = require("./NotFoundError");
+const AuthenticationError = require("./AuthenticationError");
+const AuthorizationError = require("./AuthorizationError");
 
-module.exports = { ClientError, InvariantError, NotFoundError };
+module.exports = {
+  ClientError,
+  InvariantError,
+  NotFoundError,
+  AuthenticationError,
+  AuthorizationError,
+};
