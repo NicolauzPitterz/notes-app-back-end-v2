de4c465c569902b7b2c635dceec02d9ff81fb05e
Pitters Nicolaus Tulungen <nicolauzpitterz@gmail.com>
2023-01-20 09:51:09 +0800

    authentication implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -2,6 +2,7 @@ const { nanoid } = require("nanoid");
 const { Pool } = require("pg");
 const bcrypt = require("bcrypt");
 const { InvariantError, NotFoundError } = require("../../exceptions");
+const AuthenticationError = require("../../exceptions/AuthenticationError");
 
 class UsersService {
   constructor() {
@@ -55,6 +56,29 @@ class UsersService {
     }
 
     return rows[0];
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: "SELECT id, password FROM users WHERE username = $1",
+      values: [username],
+    };
+
+    const { rows } = await this._pool.query(query);
+
+    if (!rows.length) {
+      throw new AuthenticationError("Kredensial yang Anda berikan salah");
+    }
+
+    const { id, password: hashedPassword } = rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError("Kredensial yang Anda berikan salah");
+    }
+
+    return id;
   }
 }
 
